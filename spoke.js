744f8b3d5109e020c03b4ebe6da272b82767baf8
Alan Glennon <glennon@gmail.com>
2016-04-20 12:50:08 -0700

    Update spoke.js
    add Apache license test

--- spoke.js
+++ spoke.js
@@ -1,5 +1,21 @@
+// Copyright 2016 Arogi, Inc.
 //
-// this has a dependency on leaflet.js
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+// So much documentation for such a tiny function!
+//
+// this function has a dependency on leaflet.js
 //
 // inputs:
 // hubPoint = a coordinate pair for a hub
